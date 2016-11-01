32d0e359934e548b4b7551db5b752ce08977946b
chenrushan <chenrushan@dingfudata.com>
2016-11-01 14:53:26 +0800

    add how_long_to_lock_a_mutex

---
+++ how_long_to_lock_a_mutex/how_long_to_lock_a_mutex.cpp
@@ -0,0 +1,49 @@
+// ============================================================
+// File Name : how_long_to_lock_a_mutex.cpp
+// Creation Date : 2016-11-01
+// Last Modified : Tue 01 Nov 2016 02:43:08 PM CST
+// Created By : ChenRushan
+// ============================================================
+
+#include <mutex>
+#include <boost/algorithm/string.hpp>
+#include <boost/utility/string_ref.hpp>
+#include <chrono>
+
+class Timing {
+public:
+    ~Timing() {
+        auto then = std::chrono::high_resolution_clock::now();
+        auto t = std::chrono::duration<double, std::micro>(then - now_).count();
+        fprintf(stderr, "[%s]: %lf\n", msg_.data(), t);
+        if (timing_) {
+            *timing_ = t;
+        }
+    }
+    Timing(boost::string_ref msg) : msg_(msg) {
+        now_ = std::chrono::high_resolution_clock::now();
+    }
+    Timing(boost::string_ref msg, double &timing)
+        : msg_(msg), timing_(&timing) {
+        now_ = std::chrono::high_resolution_clock::now();
+    }
+
+private:
+    double *timing_ = nullptr;
+    boost::string_ref msg_;
+    std::chrono::high_resolution_clock::time_point now_;
+};
+
+int main(int argc, char *argv[]) {
+    std::mutex m;
+    std::array<int, 100> arr;
+    {
+        Timing t("time");
+        // std::lock_guard<std::mutex> lock(m);
+        for (auto i = 0; i < arr.size(); ++i) {
+            arr[i] = i;
+        }
+    }
+    std::cout << arr[0] << std::endl;
+    return 0;
+}
